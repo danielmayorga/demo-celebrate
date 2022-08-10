0807f6450593238ce3361dad7b2a3d2c42ea60bf
Daniel Mayorga <danielmayorga@daniels-air.lan>
2022-08-09 21:26:41 -0700

    celebrate page to upload to netlify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+import { useEffect, useState } from 'react';
+
+function NavBar(){
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            QR Code
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
 
 function App() {
+  const [loginToken, setLoginToken] = useState("");
+
+  useEffect(() =>{
+    const params = new URLSearchParams(window.location.search);
+    setLoginToken(params.get("loginToken")??"no login token");
+  },[setLoginToken]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <NavBar/>
+      <Box>
+        <p style={{textAlign: "center"}}>Login Token - {loginToken}</p>
+        <div style={{display: "flex", justifyContent: "center", alignItems:"center", minHeight: "50vh"}}>
+          <div><Button variant="outlined" onClick={() => confetti()}>Celebrate</Button></div>
+        </div>
+      </Box>
+    </Box>
   );
 }
 
